c6749803afefa77e7e481c0dfb5f862beea77130
Ezefeola <ezefeola@gmail.com>
2024-11-13 19:53:17 -0300

    Last Changes

---
+++ Reffindr.Api/Middleware/TimingMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+
+namespace Reffindr.Api.Middleware;
+
+public class TimingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public TimingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var stopwatch = Stopwatch.StartNew();
+
+        await _next(context);
+
+        stopwatch.Stop();
+        Console.WriteLine($"Tiempo de ejecución de la solicitud: {stopwatch.ElapsedMilliseconds} ms");
+    }
+}
