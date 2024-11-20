97a9025fe5f3f297c9c4fc89664366cf649ea9a0
Ezefeola <ezefeola@gmail.com>
2024-11-20 20:25:42 -0300

    Program.cs fixed, userSecrets commented

--- Reffindr.Infrastructure/Migrations/20241120231118_Initial.Designer.cs
+++ Reffindr.Infrastructure/Migrations/20241120231118_Initial.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Reffindr.Infrastructure.Data;
@@ -11,9 +12,11 @@ using Reffindr.Infrastructure.Data;
 namespace Reffindr.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241120231118_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
