858aa34369c9551a411722a0ec1b905b26de5fe5
Jamir Gutierrez <106436201+JamirGDC@users.noreply.github.com>
2024-11-27 22:06:36 +0000

    cambios nuevos (#71)

--- Reffindr.Application/Services/Classes/PropertiesService.cs
+++ Reffindr.Application/Services/Classes/PropertiesService.cs
@@ -64,7 +64,7 @@ public class PropertiesService : IPropertiesService
 
         await _NotifService.AddNotificationToUser(ownerEmail, registeredProperty.Id, NotificationType.Application, cancellationToken);
 
-        PropertyPostResponseDto propertyPostResponseDto = registeredProperty.ToResponsePost();
+        PropertyPostResponseDto propertyPostResponseDto = registeredProperty.ToPostResponse();
 
         return propertyPostResponseDto;
     }
--- Reffindr.Application/Services/Classes/UserService.cs
+++ Reffindr.Application/Services/Classes/UserService.cs
@@ -15,4 +15,41 @@ public class UserService : IUserService
         _unitOfWork = unitOfWork;
     }
 
+
+    public async Task<UserUpdateResponseDto> UpdateUserAsync(UserUpdateRequestDto userRequestDto, CancellationToken cancellationToken)
+    {
+        int userId = _userContext.GetUserId();
+
+        User user = await _usersRepository.GetUserById(userId);
+
+        if (user == null)
+        {
+            throw new ArgumentNullException(nameof(user), "User cannot be null.");
+        }
+
+        user.Name = userRequestDto.Name ?? user.Name;
+        user.LastName = userRequestDto.LastName ?? user.LastName;
+        user.Dni = userRequestDto.Dni ?? user.Dni;
+        user.Phone = userRequestDto.Phone ?? user.Phone;
+        user.Address = userRequestDto.Address ?? user.Address;
+        user.BirthDate = userRequestDto.BirthDate ?? user.BirthDate;
+        user.UpdatedAt = DateTime.UtcNow;
+
+        user.IsProfileComplete = !string.IsNullOrWhiteSpace(user.Name) &&
+                             !string.IsNullOrWhiteSpace(user.LastName) &&
+                             !string.IsNullOrWhiteSpace(user.Dni) &&
+                             !string.IsNullOrWhiteSpace(user.Phone) &&
+                             !string.IsNullOrWhiteSpace(user.Address) &&
+                             user.BirthDate.HasValue;
+
+        await _unitOfWork.Complete(cancellationToken);
+
+        UserUpdateResponseDto userUpdateResponseDto = user.ToResponse();
+
+        return userUpdateResponseDto;
+
+    }
+
+
+
 }
